a41392280a11d41d6cc5c34af13a53f4e5141367
Gogs <gogs@fake.local>
2016-12-19 23:16:32 -0700

    Added tests

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+setup(
+    name="tx-webhook-client",
+    version="1.0.0",
+    description="tX webhook client",
+    long_description="tX webhook client"
+    url="https://github.com/unfoldingWord-dev/tx-webhook-client",
+    author="unfoldingWord",
+    author_email="info@door43.org",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords=["tx", "client"],
+    packages=find_packages(),
+    install_requires=["future", "requests"],
+    test_suite="tests"
+)
